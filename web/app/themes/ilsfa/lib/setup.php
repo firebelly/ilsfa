a250bbd7832cbb41176358ba1435d202b33eb9ea
Nate Beaty <nate@firebellydesign.com>
2019-01-07 18:06:00 -0600

    register utility navigation, remove unused post-format support

--- web/app/themes/ilsfa/lib/setup.php
+++ web/app/themes/ilsfa/lib/setup.php
@@ -27,7 +27,8 @@ function setup() {
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage')
+    'primary_navigation' => __('Primary Navigation', 'sage'),
+    'utility_navigation' => __('Utility Navigation', 'sage')
   ]);
 
   // Enable post thumbnails
@@ -38,7 +39,7 @@ function setup() {
 
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+  // add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
 
   // Enable HTML5 markup support
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
