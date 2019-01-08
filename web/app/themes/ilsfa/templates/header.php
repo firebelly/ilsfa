70e853ad41454796b3ceea9b54113343a6a120d2
Nate Beaty <nate@firebellydesign.com>
2019-01-07 18:06:17 -0600

    start building out nav html

--- web/app/themes/ilsfa/templates/header.php
+++ web/app/themes/ilsfa/templates/header.php
@@ -1,12 +1,22 @@
-<header class="site-header" role="banner">
-  <div class="container">
-    <h1><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
-    <nav class="site-nav" role="navigation">
+<header class="site-header">
+  <div class="wrap">
+    <h1 class="brand"><a href="<?= esc_url(home_url('/')); ?>">
+      <svg class="icon icon-logo" aria-hidden="true"><use xlink:href="#icon-logo"/></svg>
+      <span class="name"><?= get_bloginfo('name'); ?></span>
+    </a></h1>
+    <nav class="site-nav">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'primary-nav']);
+      endif;
+      if (has_nav_menu('utility_navigation')) :
+        wp_nav_menu(['theme_location' => 'utility_navigation', 'menu_class' => 'utility-nav']);
       endif;
       ?>
+      <div class="nav-search">
+        <?= get_search_form() ?>
+        <a href="#" class="search-close button"><svg class="icon icon-x" aria-hidden="true"><use xlink:href="#icon-x"/></svg></a>
+      </div>
     </nav>
   </div>
 </header>
