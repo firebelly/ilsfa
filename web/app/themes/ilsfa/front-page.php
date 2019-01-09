0dc47635065a96fcab7bd4cba943e17bdbad4463
Nate Beaty <nate@firebellydesign.com>
2019-01-09 17:57:15 -0600

    start populating homepage content with highlight/action/overview blocks, add page header content

--- web/app/themes/ilsfa/front-page.php
+++ web/app/themes/ilsfa/front-page.php
@@ -8,36 +8,80 @@ use Roots\Sage\Titles;
 $post_meta = get_post_meta($post->ID);
 
 // Headline set?
-if (!empty($post_meta['_cmb2_intro_headline'])) {
-  $intro_title = $post_meta['_cmb2_intro_headline'][0];
+if (!empty($post_meta['_cmb2_intro_title'])) {
+  $intro_title = $post_meta['_cmb2_intro_title'][0];
 } else {
   // Fallback to page title
   $intro_title = Titles\title();
 }
-
-// Intro body set?
-if (!empty($post_meta['_cmb2_intro_body'])) {
-  $intro_body = $post_meta['_cmb2_intro_body'][0];
-}
-
-// Intro Links?
-if (!empty(get_post_meta($post->ID, '_cmb2_intro_links', true))) {
-  $intro_link = get_post_meta($post->ID, '_cmb2_intro_links', true)[0];
-}
-
 ?>
 
 <header class="page-header">
-  <div class="banner-image" <?= \Firebelly\Media\get_header_bg($post) ?>></div>
-  <h2 class="page-title fb-container-content"><?= $intro_title; ?></h2>
+  <div class="banner-image-wrap">
+    <div class="banner-image" <?= \Firebelly\Media\get_header_bg($post) ?>></div>
+  </div>
+  <h2 class="page-title"><?= nl2br($intro_title); ?></h2>
 </header>
 
-<div class="content">
-  <?= apply_filters('the_content', $post->post_content) ?>
+<?php if (!empty($post_meta['_cmb2_intro_supporting_statement'])): ?>
+<div class="supporting-statement user-content">
+  <?= apply_filters('the_content', $post_meta['_cmb2_intro_supporting_statement'][0]) ?>
 </div>
+<?php endif; ?>
 
-<div class="page-section fb-container-md">
-  <h2 class="h1 text-center">Featured</h2>
-  <div class="stories mobile-gutter">
+<?php /* Overview Blocks */ ?>
+<?php if (!empty($post_meta['_cmb2_overview_blocks'])): ?>
+  <ul class="overview-blocks">
+    <?php foreach (unserialize($post_meta['_cmb2_overview_blocks'][0]) as $block): ?>
+      <li class="grid">
+        <div class="grid-item one-half">
+          <div class="image-wrap inset-shadow">
+            <div class="image" <?= \Firebelly\Media\get_header_bg($block['image_id'], ['size' => 'large']) ?>></div>
+          </div>
+        </div>
+        <div class="grid-item one-half">
+          <div class="text user-content">
+            <?= apply_filters('the_content', $block['body']) ?>
+          </div>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+
+<?php /* Highlight Blocks */ ?>
+<?php if (!empty($post_meta['_cmb2_highlight_blocks'])): ?>
+  <div class="highlight-blocks">
+    <h2 class="h1">How the program works</h2>
+    <ul class="grid">
+    <?php foreach (unserialize($post_meta['_cmb2_highlight_blocks'][0]) as $block): ?>
+        <li class="grid-item one-third">
+          <div class="icon-<?= $block['icon'] ?>"><?= $block['icon'] ?></div>
+          <div class="user-content">
+            <?= apply_filters('the_content', $block['body']) ?>
+          </div>
+        </li>
+    <?php endforeach; ?>
+    </ul>
   </div>
-</div>
+<?php endif; ?>
+
+<?php /* Action Blocks */ ?>
+<?php if (!empty($post_meta['_cmb2_action_blocks'])): ?>
+  <div class="action-blocks">
+    <h2 class="h1">What you can do</h2>
+    <ul class="cards">
+    <?php foreach (unserialize($post_meta['_cmb2_action_blocks'][0]) as $block): ?>
+        <li<?= !empty($block['disabled_text']) ? ' class="inactive"' : '' ?>>
+          <h4><?= $block['subhead'] ?></h4>
+          <h3><?= $block['header'] ?></h3>
+          <?php if (!empty($block['disabled_text'])): ?>
+            <a href="#" class="button disabled"><?= $block['disabled_text'] ?></a>
+          <?php else: ?>
+            <a href="<?= $block['url'] ?>" class="button">More</a>
+          <?php endif ?>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+  </div>
+<?php endif; ?>
