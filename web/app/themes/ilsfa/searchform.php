4eab06a03f1f86a4382f8a6995c9222984f6d0a7
Nate Beaty <nate@firebellydesign.com>
2019-01-09 17:56:47 -0600

    update searchform html for design

--- web/app/themes/ilsfa/searchform.php
+++ web/app/themes/ilsfa/searchform.php
@@ -1,8 +1,13 @@
 <form role="search" method="get" class="search-form" action="/">
-  <div class="input-wrap">
-    <input id="s"autocomplete="off" type="search" class="search-field form-control" placeholder="" value="" name="s" required>
-    <label for="s">Enter Keyword(s)</label>
+  <div class="search-wrap">
+    <div class="input-wrap">
+      <input id="s"autocomplete="off" type="search" class="search-field form-control" placeholder="" value="" name="s" required>
+      <label for="s">Enter Keyword(s)</label>
+    </div>
+    <div class="button-wrap">
+      <button type="submit" class="button">Enter</button>
+    </div>
   </div>
-  <button type="submit" class="button">Enter</button>
-  <svg class="icon icon-search" aria-hidden="true" role="presentation"><use xlink:href="#icon-search"/></svg>
+  <svg class="icon icon-search search-open" aria-hidden="true"><use xlink:href="#icon-search"/></svg>
+  <svg class="icon icon-x hidden search-close" aria-hidden="true"><use xlink:href="#icon-x"/></svg>
 </form>
