c9aac327722b1f3f43d0d42797130314a9fd9951
Nate Beaty <nate@clixel.com>
2019-01-12 11:53:29 -0600

    partials for announcement and event articles

---
+++ web/app/themes/ilsfa/templates/article-event.php
@@ -0,0 +1,21 @@
+<?php
+$event_post->meta = get_post_meta($event_post->ID);
+$address = unserialize($event_post->meta['_cmb2_address'][0]);
+$post_image = \Firebelly\Media\get_header_bg($event_post, ['size' => 'medium']);
+?>
+<article class="event">
+  <?php if (!empty($post_image)): ?>
+    <div class="image" <?= $post_image ?>></div>
+  <?php endif; ?>
+  <h3><?= $event_post->post_title ?></h3>
+  <ul class="details">
+  	<li class="date">
+  		<?= \Firebelly\PostTypes\Event\get_dates($event_post); ?>
+  	</li>
+  	<li class="location">
+			<?= $address['city'] ?>, <?= $address['state'] ?>
+  	</li>
+  </ul>
+
+  <a class="arrow" href="<?= $event_post->meta['_cmb2_event_url'][0] ?>" title="Event Details"><svg class="icon-arrow" aria-hidden="true"><use xlink:href="#icon-arrow"/></svg></a>
+</article>
