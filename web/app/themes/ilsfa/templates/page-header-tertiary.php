b2cfef7ffd01ee08ba004e1eb9979f7c4594461d
Nate Beaty <nate@firebellydesign.com>
2019-01-15 17:35:36 -0600

    add drop shadow to page-header image, add tertiary page header partial with no image

--- web/app/themes/ilsfa/templates/page-header-tertiary.php
+++ web/app/themes/ilsfa/templates/page-header-tertiary.php
@@ -11,24 +11,15 @@ if (!empty($post_meta['_cmb2_intro_title'])) {
   // Fallback to page title
   $intro_title = Titles\title();
 }
-
-// Featured image? (can send along $text_only=1 to just use text for header, e.g. Single Program)
-$has_featured_image = empty($text_only) && has_post_thumbnail($post);
 ?>
-
-<header class="page-header <?= $has_featured_image ? 'has-image' : '' ?>">
+<header class="page-header tertiary">
   <div class="title-wrap">
     <h1 class="page-title"><?= nl2br($intro_title); ?></h1>
   </div>
-  <div class="jump-to">Jump To <svg class="icon icon-arrow" aria-hidden="true"><use xlink:href="#icon-arrow"/></svg></div>
-  <?php if ($has_featured_image): ?>
-    <div class="image-wrap">
-      <div class="image -shadow" <?= \Firebelly\Media\get_header_bg($post) ?>></div>
-    </div>
-  <?php endif ?>
   <?php if (!empty($post_meta['_cmb2_intro_supporting_statement'])): ?>
     <div class="supporting-statement user-content">
       <?= apply_filters('the_content', $post_meta['_cmb2_intro_supporting_statement'][0]) ?>
     </div>
   <?php endif; ?>
+  <div class="jump-to">Jump To <svg class="icon icon-arrow" aria-hidden="true"><use xlink:href="#icon-arrow"/></svg></div>
 </header>
