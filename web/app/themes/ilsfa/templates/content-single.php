d4473683a46a680fe1a367a15b30d85d1b8382ba
Nate Beaty <nate@clixel.com>
2019-01-17 16:45:47 -0600

    update single pages templates to pick up global styles

--- web/app/themes/ilsfa/templates/content-single.php
+++ web/app/themes/ilsfa/templates/content-single.php
@@ -1,15 +1,23 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
+  <?php
+  $post_meta = get_post_meta($post->ID);
+  get_template_part('templates/page', 'header-tertiary');
+  ?>
+  <div class="page-content user-content">
+
+    <article <?php post_class(); ?>>
+      <div class="entry-meta">
+        <?php get_template_part('templates/entry-meta'); ?>
+      </div>
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div>
+      <footer>
+        <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+      </footer>
+      <?php comments_template('/templates/comments.php'); ?>
+      <?php \Firebelly\Utils\get_template_part_with_vars('templates/resources', 'list', ['post_meta' => $post_meta]); ?>
+    </article>
+
+  </div>
 <?php endwhile; ?>
