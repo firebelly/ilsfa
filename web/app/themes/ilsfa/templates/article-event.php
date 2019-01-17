3094955b153d0e2c751e343988683f93bf854a6d
Nate Beaty <nate@clixel.com>
2019-01-17 16:42:59 -0600

    don't bomb when events don't have address or url

--- web/app/themes/ilsfa/templates/article-event.php
+++ web/app/themes/ilsfa/templates/article-event.php
@@ -1,6 +1,8 @@
 <?php
-$event_post->meta = get_post_meta($event_post->ID);
-$address = unserialize($event_post->meta['_cmb2_address'][0]);
+$event_post_meta = get_post_meta($event_post->ID);
+if (!empty($event_post_meta['_cmb2_address'])) {
+  $address = unserialize($event_post_meta['_cmb2_address'][0]);
+}
 $post_image = \Firebelly\Media\get_header_bg($event_post, ['size' => 'medium']);
 ?>
 <article class="event">
@@ -9,15 +11,18 @@ $post_image = \Firebelly\Media\get_header_bg($event_post, ['size' => 'medium']);
   <?php endif; ?>
   <h3><?= $event_post->post_title ?></h3>
   <ul class="icon-list details -small">
-  	<li class="date">
+    <li class="date">
       <svg class="icon icon-date" aria-hidden="true"><use xlink:href="#icon-date"/></svg>
-  		<?= \Firebelly\PostTypes\Event\get_dates($event_post); ?>
-  	</li>
-  	<li class="location">
-      <svg class="icon icon-location" aria-hidden="true"><use xlink:href="#icon-location"/></svg>
-			<?= $address['city'] ?><?= !empty($address['state']) ? ', '.$address['state'] : '' ?>
-  	</li>
+      <?= \Firebelly\PostTypes\Event\get_dates($event_post); ?>
+    </li>
+    <?php if (!empty($address)): ?>
+      <li class="location">
+        <svg class="icon icon-location" aria-hidden="true"><use xlink:href="#icon-location"/></svg>
+        <?= $address['city'] ?><?= !empty($address['state']) ? ', '.$address['state'] : '' ?>
+      </li>
+    <?php endif; ?>
   </ul>
-
-  <a class="button -icon -round" href="<?= $event_post->meta['_cmb2_event_url'][0] ?>" title="Event Details"><svg class="icon-arrow" aria-hidden="true"><use xlink:href="#icon-arrow"/></svg></a>
+  <?php if (!empty($event_post_meta['_cmb2_event_url'])): ?>
+    <a class="button -icon -round" href="<?= $event_post_meta['_cmb2_event_url'][0] ?>" title="Event Details"><svg class="icon-arrow" aria-hidden="true"><use xlink:href="#icon-arrow"/></svg></a>
+  <?php endif ?>
 </article>
