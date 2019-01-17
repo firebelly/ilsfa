8a6be4acd916326b1f4bee31d50539c23dbc8468
Nate Beaty <nate@clixel.com>
2019-01-17 16:46:03 -0600

    omit footer-outro if is_search()

--- web/app/themes/ilsfa/templates/footer.php
+++ web/app/themes/ilsfa/templates/footer.php
@@ -1,8 +1,7 @@
 <?php
 // Check for footer_outro
-global $post_meta;
 $post_meta = !empty($post_meta) ? $post_meta : (!empty($post) && is_object($post) ? get_post_meta($post->ID) : []);
-if (!empty($post_meta['_cmb2_footer_outro'])): ?>
+if (!is_search() && !empty($post_meta['_cmb2_footer_outro'])): ?>
 <div class="footer-outro">
   <div class="user-content">
     <?= apply_filters('the_content', $post_meta['_cmb2_footer_outro'][0]) ?>
