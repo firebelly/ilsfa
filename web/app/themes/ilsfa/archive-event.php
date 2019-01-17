edd90fa73dbaba085a02574f76ba36b21a4b54d0
Nate Beaty <nate@clixel.com>
2019-01-17 16:37:07 -0600

    add and style template for events landing page

---
+++ web/app/themes/ilsfa/archive-event.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  Template name: Events
+*/
+
+// Get all post_meta
+$post = get_page_by_path('/events/');
+$post_meta = get_post_meta($post->ID);
+?>
+
+<?php
+get_template_part('templates/page', 'header-tertiary');
+?>
+
+<div class="events-listing">
+  <?php if ($events = \Firebelly\PostTypes\Event\get_events(['output' => 'array'])): ?>
+    <ul class="cards compact-grid">
+    <?php foreach ($events as $event): ?>
+      <li>
+        <?php \Firebelly\Utils\get_template_part_with_vars('templates/article', 'event', ['event_post' => $event]); ?>
+      </li>
+    <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+</div>
