35aae617dce84bdb97c489247d9b63be66c39670
Nate Beaty <nate@firebellydesign.com>
2019-01-24 15:48:02 -0600

    enter -> search, change to just “Keyword(s)” as search label on mobile

--- web/app/themes/ilsfa/searchform.php
+++ web/app/themes/ilsfa/searchform.php
@@ -2,10 +2,10 @@
   <div class="search-wrap">
     <div class="input-wrap">
       <input id="s"autocomplete="off" type="search" class="search-field form-control" placeholder="" value="" name="s" required>
-      <label for="s">Enter Keyword(s)</label>
+      <label for="s"><span class="show-for-medium-up -inline">Enter </span>Keyword(s)</label>
     </div>
     <div class="button-wrap">
-      <button type="submit" class="button">Enter</button>
+      <button type="submit" class="button">Search</button>
     </div>
   </div>
   <a class="search-close button -icon -round -small" href="#"><svg class="icon icon-cross" aria-hidden="true"><use xlink:href="#icon-cross"/></svg></a>
