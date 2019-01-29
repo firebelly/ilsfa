f8e6d81f14a248d93fed5f29ce405824d8e6e3b3
Nate Beaty <nate@clixel.com>
2019-01-29 17:34:47 -0600

    style jumpto nav for mobile; add behavior to expand/collapse jumpto nav; style jumpto nav for interim state between mobile and desktop

--- web/app/themes/ilsfa/templates/jumpto-links.php
+++ web/app/themes/ilsfa/templates/jumpto-links.php
@@ -1,6 +1,10 @@
-<div class="jump-to">
-  <div class="jump-title">
+<div class="jumpto">
+  <div class="jumpto-title">
     Jump To <svg class="icon icon-arrow" aria-hidden="true"><use xlink:href="#icon-arrow"/></svg>
   </div>
   <ul><li>&nbsp;</li></ul>
+  <a class="jumpto-toggle" href="#">
+  	<svg class="icon icon-plus" aria-hidden="true"><use xlink:href="#icon-plus"/></svg>
+  	<svg class="icon icon-minus" aria-hidden="true"><use xlink:href="#icon-minus"/></svg>
+  </a>
 </div>
