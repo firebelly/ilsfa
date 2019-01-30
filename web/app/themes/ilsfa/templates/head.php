781dae00879e83fd977cd5276e4d8a651343dd06
Nate Beaty <nate@clixel.com>
2019-01-30 17:17:10 -0600

    only show google analytics code if WP_ENV==production

--- web/app/themes/ilsfa/templates/head.php
+++ web/app/themes/ilsfa/templates/head.php
@@ -5,13 +5,15 @@
   <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png') ?>" />
   <?php wp_head(); ?>
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103560791-2"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-103560791-2');
-  </script>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <?php if (WP_ENV === 'production'): ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103560791-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-103560791-2');
+    </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+  <?php endif; ?>
 </head>
