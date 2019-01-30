f00d91cc68dd4013f7761209e150f34880b137b4
Nate Beaty <nate@clixel.com>
2019-01-30 13:05:22 -0600

    fix 404 page styling

--- web/app/themes/ilsfa/404.php
+++ web/app/themes/ilsfa/404.php
@@ -1,7 +1,5 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/page', 'header-tertiary'); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+<div class="search-large">
+  <?= get_search_form() ?>
 </div>
-
-<?php get_search_form(); ?>
