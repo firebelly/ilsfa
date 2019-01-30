691a47a01cb1701360465f3800c4b91abb7b76e6
Nate Beaty <nate@clixel.com>
2019-01-30 12:39:41 -0600

    add analytics

--- web/app/themes/ilsfa/templates/head.php
+++ web/app/themes/ilsfa/templates/head.php
@@ -4,4 +4,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png') ?>" />
   <?php wp_head(); ?>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103560791-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-103560791-2');
+  </script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
 </head>
