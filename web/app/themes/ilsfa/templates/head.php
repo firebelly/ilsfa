a78f6869a8f3c747f4a270583bf000e3b89b9241
Nate Beaty <nate@firebellydesign.com>
2019-02-04 14:45:04 -0600

    move ga embed to top of head

--- web/app/themes/ilsfa/templates/head.php
+++ web/app/themes/ilsfa/templates/head.php
@@ -1,10 +1,4 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png') ?>" />
-  <?php wp_head(); ?>
-
   <?php if (WP_ENV === 'production'): ?>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103560791-2"></script>
@@ -16,4 +10,9 @@
     </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
   <?php endif; ?>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png') ?>" />
+  <?php wp_head(); ?>
 </head>
