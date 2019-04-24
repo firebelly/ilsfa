7a7dff9b577d118310e61a704aca1ba521cf0484
Nate Beaty <nate@clixel.com>
2019-04-24 13:46:53 -0400

    use new shared function to find active regions for orgs

--- web/app/themes/ilsfa/templates/organizations-listing.php
+++ web/app/themes/ilsfa/templates/organizations-listing.php
@@ -8,21 +8,7 @@
   ]);
 
   if (is_array($org_post_ids)) {
-    // Find regions that use those post IDs
-    $org_region_ids = $wpdb->get_col("
-    SELECT t.term_id FROM {$wpdb->terms} AS t
-          INNER JOIN {$wpdb->term_taxonomy} AS tt ON t.term_id = tt.term_id
-          INNER JOIN {$wpdb->term_relationships} AS r ON r.term_taxonomy_id = tt.term_taxonomy_id
-          WHERE tt.taxonomy IN('region')
-          AND r.object_id IN (".implode(',', $org_post_ids).")
-          GROUP BY t.term_id
-    ");
-
-    // Pull those regions for filtering
-    $regions = get_terms([
-      'taxonomy' => 'region',
-      'include'  => $org_region_ids
-    ]);
+    $regions = \Firebelly\Utils\get_active_terms_for_posts($org_post_ids, 'region');
   } else {
     $regions = [];
   }
@@ -39,42 +25,40 @@
         </div>
       <?php endif; ?>
 
-      <?php if (!empty($organizations)): ?>
-        <h4>Sort By</h4>
-        <div class="select-wrap">
-          <svg class="icon icon-arrow-dropdown" aria-hidden="true"><use xlink:href="#icon-arrow-dropdown"/></svg>
-          <select name="org_sort" class="jumpselect">
-            <?php foreach ([
-              'asc' => 'Name (A-Z)',
-              'desc' => 'Name (Z-A)',
-            ] as $value => $title): ?>
-              <option <?= $org_sort == $value ? 'selected ' : '' ?>value="<?= add_query_arg('org_sort', $value) ?>#organizations"><?= $title ?></option>
-            <?php endforeach; ?>
-          </select>
-        </div>
-        <h4>Region</h4>
-        <div class="select-wrap">
-          <svg class="icon icon-arrow-dropdown" aria-hidden="true"><use xlink:href="#icon-arrow-dropdown"/></svg>
-          <select name="org_region" class="jumpselect">
-            <option <?= $org_region == '' ? 'selected ' : '' ?>value="<?= add_query_arg('org_region', '') ?>#organizations">* All *</option>
-            <?php
-            foreach ($regions as $term): ?>
-              <option <?= $org_region == $term->slug ? 'selected ' : '' ?>value="<?= add_query_arg('org_region', $term->slug) ?>#organizations"><?= $term->name ?></option>
-            <?php endforeach; ?>
-          </select>
-        </div>
+      <h4>Sort By</h4>
+      <div class="select-wrap">
+        <svg class="icon icon-arrow-dropdown" aria-hidden="true"><use xlink:href="#icon-arrow-dropdown"/></svg>
+        <select name="sort" class="jumpselect">
+          <?php foreach ([
+            'asc' => 'Name (A-Z)',
+            'desc' => 'Name (Z-A)',
+          ] as $value => $title): ?>
+            <option <?= $sort == $value ? 'selected ' : '' ?>value="<?= add_query_arg('sort', $value) ?>#organizations"><?= $title ?></option>
+          <?php endforeach; ?>
+        </select>
+      </div>
+      <h4>Region</h4>
+      <div class="select-wrap">
+        <svg class="icon icon-arrow-dropdown" aria-hidden="true"><use xlink:href="#icon-arrow-dropdown"/></svg>
+        <select name="region" class="jumpselect">
+          <option <?= $region == '' ? 'selected ' : '' ?>value="<?= add_query_arg('region', '') ?>#organizations">* All *</option>
+          <?php
+          foreach ($regions as $term): ?>
+            <option <?= $region == $term->slug ? 'selected ' : '' ?>value="<?= add_query_arg('region', $term->slug) ?>#organizations"><?= $term->name ?></option>
+          <?php endforeach; ?>
+        </select>
+      </div>
 
-        <ul class="cards" data-load-more-container>
-        <?= $organizations ?>
-        </ul>
+      <ul class="cards" data-load-more-container>
+      <?= $organizations ?>
+      </ul>
 
-        <?php if ($total_pages>1): ?>
-            <div class="load-more" data-post-type="organization" data-page-at="<?= $paged ?>" data-per-page="<?= $per_page ?>" data-total-pages="<?= $total_pages ?>" data-org-sort="<?= $org_sort ?>" data-org-type="<?= $org_type ?>" data-org-region="<?= $org_region ?>">
-              <a class="button -wide -icon-right" href="#">
-                Load More <svg class="icon icon-plus" aria-hidden="true"><use xlink:href="#icon-plus"/></svg>
-              </a>
-            </div>
-        <?php endif; ?>
+      <?php if ($total_pages>1): ?>
+          <div class="load-more" data-post-type="organization" data-page-at="<?= $paged ?>" data-per-page="<?= $per_page ?>" data-total-pages="<?= $total_pages ?>" data-org-sort="<?= $sort ?>" data-org-type="<?= $org_type ?>" data-org-region="<?= $region ?>">
+            <a class="button -wide -icon-right" href="#">
+              Load More <svg class="icon icon-plus" aria-hidden="true"><use xlink:href="#icon-plus"/></svg>
+            </a>
+          </div>
       <?php endif; ?>
     </div>
 
