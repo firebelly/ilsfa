9ccd80c752ce0f4156499f1abc1a342214203198
Nate Beaty <nate@clixel.com>
2019-05-15 13:43:05 -0400

    switch to gtag syntax for tracking outbound link

--- web/app/themes/ilsfa/assets/scripts/main.js
+++ web/app/themes/ilsfa/assets/scripts/main.js
@@ -93,15 +93,17 @@ var ILSFA = (function($) {
     // Track GA event for outbound links
     $('a[href^="http"]:not([href*="' + currentDomain + '"])').on('click', function(e) {
       // Just return if Analytics isn't initiated
-      if (typeof ga === 'undefined') {
+      if (typeof gtag === 'undefined') {
         return;
       }
-      // .. otherwise track outbound link
+      // .. otherwise cancel click & track outbound link
       e.preventDefault();
       var $this = $(this);
-      ga('send', 'event', 'outbound', 'click', $this.attr('href'), {
-        'transport': 'beacon',
-        'hitCallback': function() {
+      gtag('event', 'click', {
+        'event_category': 'outbound',
+        'event_label': $this.attr('href'),
+        'transport_type': 'beacon',
+        'event_callback': function(){
           // Open in new tab/window if specified
           if ($this.attr('target')) {
             window.open(url, $this.attr('target'));
